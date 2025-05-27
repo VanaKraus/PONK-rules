be0f01e16dc2089c2f5bf1d17fce5f03eeceaa7e
Ivan Kraus <ivakra@centrum.cz>
2025-05-27 14:46:54 +0200

    Add non-institutional rules profile

--- server/profiles.py
+++ server/profiles.py
@@ -1,4 +1,5 @@
 from document_applicables.rules import Rule
+from document_applicables.metrics import Metric
 from document_applicables.rules.acceptability import (
     RuleDoubleComparison,
     RuleWrongValencyCase,
@@ -88,6 +89,53 @@ def get_minimal_rules() -> list[Rule]:
     ]
 
 
+def get_noninstitutional_rules() -> list[Rule]:
+    return [
+        RulePassive(),
+        RulePredSubjDistance(max_distance=5),
+        RulePredObjDistance(max_distance=4),
+        RuleMultiPartVerbs(max_distance=4),
+        RuleLongSentences(max_length=22),
+        RulePredAtClauseBeginning(max_order=4),
+        RuleVerbalNouns(),
+        RuleTooFewVerbs(min_verb_frac=0.169),
+        RuleTooManyNegations(
+            max_allowable_negations=2,
+            max_negation_frac=0.215,  # measurable effect counter-intuitive
+        ),
+        RuleWeakMeaningWords(),
+        RuleAbstractNouns(),
+        RuleRelativisticExpressions(),
+        RuleConfirmationExpressions(),
+        RuleRedundantExpressions(),
+        RuleTooLongExpressions(),
+        RuleAnaphoricReferences(),
+        RuleTooManyNominalConstructions(max_allowable_nouns=5, max_noun_frac=0.728),
+        RuleCaseRepetition(max_repetition_count=3),
+        RuleGPcoordovs(),
+        RuleGPdeverbaddr(),
+        RuleGPpatinstr(),
+        RuleGPdeverbsubj(),
+        RuleGPadjective(),
+        RuleGPpatbenperson(),
+        RuleGPwordorder(),
+        RuleReflexivePassWithAnimSubj(),
+        RuleLiteraryStyle(),
+        # RuleDoubleAdpos(max_allowable_distance=0), # counter-intuitive measurable effect
+        # RuleInfVerbDistance(max_distance=0), # counter-intuitive measurable effect
+        # RuleAmbiguousRegards(), # unreliable
+        # RuleDoubleComparison(), # unreliable + acceptability
+        # RuleWrongValencyCase(), # unreliable + acceptability
+        # RuleWrongVerbonominalCase(), # unreliable + acceptability
+        # RuleIncompleteConjunction(), # acceptability
+        # RuleFunctionWordRepetition(), # unreliable
+    ]
+
+
+def get_noninstitutional_metrics() -> list[Metric]:
+    raise NotImplementedError()
+
+
 def set_rules_verbose(rules: list[Rule]) -> list[Rule]:
     for rule in rules:
         rule.verbose_annotation = True
@@ -96,6 +144,7 @@ def set_rules_verbose(rules: list[Rule]) -> list[Rule]:
 
 profiles = {
     'default': (None, None),
+    'noninstitutional': (get_noninstitutional_metrics(), get_noninstitutional_rules()),
     'minimal': (
         None,  # default metrics
         get_minimal_rules(),
