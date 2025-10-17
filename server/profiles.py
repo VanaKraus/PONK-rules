e72151ae80e6d42735a18d774753c808ea985d81
Ivan Kraus <ivakra@centrum.cz>
2025-10-17 23:18:56 +0200

    add note

--- server/profiles.py
+++ server/profiles.py
@@ -93,7 +93,7 @@ def get_minimal_rules() -> list[Rule]:
 
 
 def get_noninstitutional_rules() -> list[Rule]:
-    # TODO: allow RuleIncompleteConstruction
+    # TODO: allow RuleIncompleteConstruction & RuleDoubleAdpos
     return [
         # --- fluency ----
         # RuleTooFewVerbs(min_verb_frac=0.169), # value counter-intuitive; replaced below
