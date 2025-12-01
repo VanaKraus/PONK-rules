7dfca8d5d9965a6f9016f09c301520e3dbf4f4ad
Ivan Kraus <ivakra@centrum.cz>
2025-12-01 17:36:43 +0100

    uncomment derinet

--- document_applicables/rules/clusters.py
+++ document_applicables/rules/clusters.py
@@ -401,10 +401,8 @@ class RulePassive(ClusterRule):
         ]
 
         # look up the verb in VALLEX
-        # FIXME: uncomment (deployment issues with DeriNet)
-        # derinet = get_derinet()
-        # deri_parents = [lx.parent.lemma if lx.parent else None for lx in derinet.get_lexemes(participle.lemma)]
-        deri_parents = []
+        derinet = get_derinet()
+        deri_parents = [lx.parent.lemma if lx.parent else None for lx in derinet.get_lexemes(participle.lemma)]
         vallex_lexemes = [l for dp in deri_parents if dp for l in vallex_get_lexeme(dp)]
 
         # if there's a VALLEX entry
