2988baf3fe4b23462d40f9ddb1f8958c7c2fba10
Ivan Kraus <ivakra@centrum.cz>
2025-12-08 16:39:57 +0100

    make rule name consistent with others

--- document_applicables/rules/sentence_position.py
+++ document_applicables/rules/sentence_position.py
@@ -227,7 +227,7 @@ class RuleMultiPartVerbs(SentencePositionRule):
                 self.advance_application_id()
 
 
-class PredTooFarInClause(SentencePositionRule):
+class RulePredTooFarInClause(SentencePositionRule):
     """Capture predicates (their finite tokens for multi-token predicates) \
         that are too far from the beginning of their clause.
 
--- server/api_helpers.py
+++ server/api_helpers.py
@@ -61,7 +61,7 @@ from document_applicables.rules.sentence_position import (
     RulePredObjDistance,
     RuleInfVerbDistance,
     RuleMultiPartVerbs,
-    PredTooFarInClause,
+    RulePredTooFarInClause,
 )
 
 
