188352a4492faa1fe1893af82e90673d80ca07ca
Ivan Kraus <ivakra@centrum.cz>
2025-12-31 14:30:16 +0100

    tweak DoubleAdpos

--- document_applicables/rules/ambiguity.py
+++ document_applicables/rules/ambiguity.py
@@ -21,7 +21,9 @@ from document_applicables.rules.util.structure_retrieval import (
     get_clause,
     get_clause_root,
     get_surrounding_bundles_serialize,
+    get_phrase_heads,
 )
+from document_applicables.rules.util.measurement import distance_from_list
 from document_applicables.rules.util.external_tools import morphodita_generate
 
 
@@ -81,9 +83,11 @@ class RuleDoubleAdpos(AmbiguityRule):
         return bool(cls._adpositions(node))
 
     def process_node(self, node: Node):
-        if self._has_adposition(node):
+        if self._has_adposition(node) and not is_citation(node):
             # get all tokens the node is coordinated with (i.e. the whole coordination)
             coordinations = [c for c in node.children if c.deprel == 'conj' and c.feats['Case'] == node.feats['Case']]
+
+            phrase_heads = get_phrase_heads(node.root.descendants, keep=[node] + coordinations)
 
             # these will point to last element with an adposition throughout iterating
             ref_el, ref_adp = node, self._adpositions(node)[-1]
@@ -96,14 +100,11 @@ class RuleDoubleAdpos(AmbiguityRule):
                 # if tokens without an adposition visited previously
                 if len(coord_chain) > 1:
                     adp_highlight = ref_adp.descendants(add_self=True)
-                    element_phrases = [
-                        nd
-                        for el in coord_chain
-                        for nd in get_coord_element_phrase(el)
-                        if nd not in adp_highlight and not (nd.lemma == '.' and nd.parent.deprel == 'root')
-                    ]
-                    cconj_highlight = [nd for nd in element_phrases if nd.deprel == 'cc']
-                    el_highlight = [nd for nd in element_phrases if nd not in cconj_highlight]
+
+                    # so that all elements of the coordination are highlighted,
+                    # although the rule may only have been triggered by the last one
+                    el_highlight = [c for c in [node] + coordinations if c >= coord_chain[0] and c <= coord_chain[-1]]
+                    cconj_highlight = [nd for c in el_highlight for nd in c.children if nd.deprel == 'cc']
 
                     self.annotate_node('orig_adpos', *adp_highlight)
                     self.annotate_node('coord_el', *el_highlight)
@@ -135,8 +136,12 @@ class RuleDoubleAdpos(AmbiguityRule):
                     ref_el, ref_adp = coord, adps[-1]
                     coord_chain = [ref_el]
 
-                # token has no adposition and is too far from the reference element
-                elif coord.ord - ref_el.ord > self.max_allowable_distance:
+                # token has no adposition and is too far from the reference element and isn't a citation
+                elif (
+                    distance_from_list(phrase_heads, ref_el, coord) > self.max_allowable_distance
+                    and not is_citation(coord)
+                    and ref_adp.lemma not in {'mezi', 'in'}  # exceptions
+                ):
                     coord_chain += [coord]
 
             attempt_coord_chain_annotation()
